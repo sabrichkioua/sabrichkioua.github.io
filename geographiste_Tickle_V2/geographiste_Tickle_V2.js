0eec9562400fd4cf688f69d6f71ec7bb7857609d
sabri <sabri.chkioua@gmail.com>
2022-03-22 00:52:58 +0100

    ajout dossier "geographiste_Tickle_V2"

---
+++ geographiste_Tickle_V2/geographiste_Tickle_V2.js
@@ -0,0 +1,154 @@
+/*
+ * @name Tickle
+ * @description The word "tickle" jitters when the cursor hovers over.
+ * Sometimes, it can be tickled off the screen.
+ */
+let message = 'GéoGraphiste',
+  font,
+  bounds, // holds x, y, w, h of the text's bounding box
+  fontsize = 50,
+  x,
+  y; // x and y coordinates of the text
+
+//***************************
+let numBalls = 13;
+let spring = 0.05;
+let gravity = 0.03;
+let friction = -0.9;
+let balls = [];
+//***************************
+
+
+
+
+
+function preload() {
+  //font = loadFont('data/SourceSansPro-Regular.otf');
+  font = loadFont('data/NimbusSanL-Bol.otf');
+}
+
+
+function setup() {
+  createCanvas(900, 150);
+  //***************************
+  for (let i = 0; i < numBalls; i++) {
+    balls[i] = new Ball(
+      random(width),
+      random(height),
+      random(30, 70),
+      i,
+      balls
+    );
+  }
+  noStroke();
+  fill(255, 200);
+  //***************************
+  
+
+  // set up the font
+  textFont(font);
+  textSize(fontsize);
+  couleur_police_bleu = color('#2280bd');
+
+  // get the width and height of the text so we can center it initially
+  bounds = font.textBounds(message, 0, 0, fontsize);
+  x = width / 2 - bounds.w / 2;
+  y = height / 2 - bounds.h / 2;
+}
+
+function draw() {
+  background(255, 255);
+  //***************************
+  balls.forEach(ball => {
+    ball.collide();
+    ball.move();
+    ball.display();
+  });  
+  //***************************
+
+  // write the text in black and get its bounding box
+  fill(couleur_police_bleu);
+  text(message, x, y);
+  bounds = font.textBounds(message, x, y, fontsize);
+
+  // check if the mouse is inside the bounding box and tickle if so
+  if (
+    mouseX >= bounds.x &&
+    mouseX <= bounds.x + bounds.w &&
+    mouseY >= bounds.y &&
+    mouseY <= bounds.y + bounds.h
+  ) {
+    x += random(-5, 5);
+    y += random(-5, 5);
+  }
+}
+
+
+
+
+
+
+  //***************************
+    //***************************
+
+
+
+class Ball {
+  constructor(xin, yin, din, idin, oin) {
+    this.x = xin;
+    this.y = yin;
+    this.vx = 0;
+    this.vy = 0;
+    this.diameter = din;
+    this.id = idin;
+    this.others = oin;
+  }
+
+  collide() {
+    for (let i = this.id + 1; i < numBalls; i++) {
+      // console.log(others[i]);
+      let dx = this.others[i].x - this.x;
+      let dy = this.others[i].y - this.y;
+      let distance = sqrt(dx * dx + dy * dy);
+      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+      //   console.log(distance);
+      //console.log(minDist);
+      if (distance < minDist) {
+        //console.log("2");
+        let angle = atan2(dy, dx);
+        let targetX = this.x + cos(angle) * minDist;
+        let targetY = this.y + sin(angle) * minDist;
+        let ax = (targetX - this.others[i].x) * spring;
+        let ay = (targetY - this.others[i].y) * spring;
+        this.vx -= ax;
+        this.vy -= ay;
+        this.others[i].vx += ax;
+        this.others[i].vy += ay;
+      }
+    }
+  }
+
+  move() {
+    this.vy += gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x + this.diameter / 2 > width) {
+      this.x = width - this.diameter / 2;
+      this.vx *= friction;
+    } else if (this.x - this.diameter / 2 < 0) {
+      this.x = this.diameter / 2;
+      this.vx *= friction;
+    }
+    if (this.y + this.diameter / 2 > height) {
+      this.y = height - this.diameter / 2;
+      this.vy *= friction;
+    } else if (this.y - this.diameter / 2 < 0) {
+      this.y = this.diameter / 2;
+      this.vy *= friction;
+    }
+  }
+
+  display() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+}
