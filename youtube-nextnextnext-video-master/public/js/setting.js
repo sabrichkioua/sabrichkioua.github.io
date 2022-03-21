1559e9cc203710e8785b60d5cd62e7f2d6b90d32
sabri <sabri.chkioua@gmail.com>
2022-03-22 00:20:03 +0100

    suppression dossier "youtube-nextnextnext-video-master"

--- youtube-nextnextnext-video-master/public/js/setting.js
+++
@@ -1,7 +0,0 @@
-
-function setColorSN(setNum, opacity) {
-  let setColor = color(visual.hueArr[setNum], visual.staurationArr[setNum], visual.brightnessArr[setNum], opacity);
-    return setColor;
-}
-
-
